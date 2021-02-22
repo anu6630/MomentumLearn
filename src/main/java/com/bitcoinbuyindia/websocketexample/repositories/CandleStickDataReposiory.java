176a951e773d7a38ea3013fa1a0be1bcecabfde0
Anuraj T S <anu6630@gmail.com>
2021-02-22 17:47:40 +0530

    remove all files

--- src/main/java/com/bitcoinbuyindia/websocketexample/repositories/CandleStickDataReposiory.java
+++
@@ -1,8 +0,0 @@
-package com.bitcoinbuyindia.websocketexample.repositories;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.bitcoinbuyindia.websocketexample.entities.CandleStickData;
-@Repository
-public interface CandleStickDataReposiory extends CrudRepository<CandleStickData,Long> {
-	
-}
